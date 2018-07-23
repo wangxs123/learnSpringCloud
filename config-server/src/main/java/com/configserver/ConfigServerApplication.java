36abb0bc7342184720d86e6dd39f73de65edfcf8
wangxsaz@dingtalk.com <wangxsaz@dingtalk.com>
2018-07-23 09:24:55 +0800

    11

---
+++ config-server/src/main/java/com/configserver/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author wangxs
+ * @date 2018-07-13
+ * @EnableConfigServer注解开启配置服务器的功能
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
