1cf68d4cc4ca4eba0147ce863751866f6db94eb6
wangxsaz@dingtalk.com <wangxsaz@dingtalk.com>
2018-07-23 09:24:23 +0800

    11

--- service-feign/src/main/java/com/servicefeign/service/SchedualServiceHi.java
+++ service-feign/src/main/java/com/servicefeign/service/SchedualServiceHi.java
@@ -1,5 +1,6 @@
 package com.servicefeign.service;
 
+import com.servicefeign.controller.SchedualServiceHiHystric;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author： 王孝双
  * @date：Create by 2018/7/12 14:16
  */
-@FeignClient(value = "service-hi")
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
